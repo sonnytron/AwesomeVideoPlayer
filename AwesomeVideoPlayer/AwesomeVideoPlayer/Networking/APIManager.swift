79178fb31d9f812c7fe169ae0c0b072000e3d468
sonny <sonny@linecorp.com>
2018-08-01 21:45:39 +0900

    APIManager to download video objects

---
+++ AwesomeVideoPlayer/AwesomeVideoPlayer/Networking/APIManager.swift
@@ -0,0 +1,32 @@
+//
+//  APIManager.swift
+//  AwesomeVideoPlayer
+//
+//  Created by Sonny on 2018/08/01.
+//  Copyright © 2018 Sonny. All rights reserved.
+//
+
+import Foundation
+
+final class APIManager {
+    static func reloadVideos(completion: @escaping (_ videos: [NetworkVideo]?, _ error: Error?) -> Void) {
+        let videoUrl = URL(string: "https://quipper.github.io/native-technical-exam/playlist.json")
+        guard let url = videoUrl else { return }
+        URLSession.shared.dataTask(with: url) { (data, response, error) in
+            if let error = error, data == nil {
+                completion(nil, error)
+                print("Error during network call: \(error.localizedDescription)")
+            }
+            if let data = data {
+                do {
+                    let networkVideos = try JSONDecoder().decode([NetworkVideo].self, from: data)
+                    print("We have Videos: \(networkVideos)")
+                    completion(networkVideos, nil)
+                } catch let error {
+                    completion(nil, error)
+                    print("Error during decoding: \(error.localizedDescription)")
+                }
+            }
+        }.resume()
+    }
+}
