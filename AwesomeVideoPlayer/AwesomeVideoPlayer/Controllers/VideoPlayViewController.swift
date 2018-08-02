6a7e4cc0e3a69412c0e31b5379a46baf091fa71e
sonny <sonny@linecorp.com>
2018-08-02 14:30:25 +0900

    Add video play view controller without UI or code to project

---
+++ AwesomeVideoPlayer/AwesomeVideoPlayer/Controllers/VideoPlayViewController.swift
@@ -0,0 +1,19 @@
+//
+//  VideoPlayViewController.swift
+//  AwesomeVideoPlayer
+//
+//  Created by ST20894 on 2018/08/02.
+//  Copyright © 2018 Sonny. All rights reserved.
+//
+
+import UIKit
+
+class VideoPlayViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
