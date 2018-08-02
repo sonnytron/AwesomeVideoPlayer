093ade5a4f98e9d78d6fa6eb2a72946010b88a9c
sonny <sonny@linecorp.com>
2018-08-02 15:12:46 +0900

    Built video view, video view controller, rotation works

--- AwesomeVideoPlayer/AwesomeVideoPlayer/Controllers/VideoPlayViewController.swift
+++ AwesomeVideoPlayer/AwesomeVideoPlayer/Controllers/VideoPlayViewController.swift
@@ -7,13 +7,37 @@
 //
 
 import UIKit
+import AVFoundation
 
 class VideoPlayViewController: UIViewController {
+    
+    let playerView: AVPlayer
+    @IBOutlet var videoView: VideoView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        guard let playerLayer = videoView.layer as? AVPlayerLayer else {
+            return
+        }
+        playerLayer.player = playerView
+        playerLayer.videoGravity = .resizeAspect
+        playerView.play()
+    }
+    
+    init(withVideo video: NetworkVideo) {
+        guard let videoURL = URL(string: video.videoURL) else {
+            fatalError("Failed to load video URL: \(video.videoURL)")
+        }
+        playerView = AVPlayer(url: videoURL)
+        super.init(nibName: "VideoPlayViewController", bundle: nil)
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init coder aDecoder: not implemented")
     }
 
 }
